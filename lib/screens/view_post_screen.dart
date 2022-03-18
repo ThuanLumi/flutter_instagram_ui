33d04fb79da8bf69c93a4eb1e740a7ffbc3f7b76
Thuan Dinh Nam <thuandinhnam307@gmail.com>
2022-03-18 16:47:48 +0700

    Add flutter_template_instagram_ui folder

---
+++ lib/screens/view_post_screen.dart
@@ -0,0 +1,320 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_template_instagram_ui/models/comment_model.dart';
+import 'package:flutter_template_instagram_ui/models/post_model.dart';
+
+class ViewPostScreen extends StatefulWidget {
+  final Post post;
+
+  ViewPostScreen({required this.post});
+  @override
+  _ViewPostScreenState createState() => _ViewPostScreenState();
+}
+
+class _ViewPostScreenState extends State<ViewPostScreen> {
+  Widget _buildComment(int index) {
+    return Padding(
+      padding: EdgeInsets.all(10.0),
+      child: ListTile(
+        leading: Container(
+          width: 50.0,
+          height: 50.0,
+          decoration: BoxDecoration(
+            shape: BoxShape.circle,
+            boxShadow: [
+              BoxShadow(
+                color: Colors.black45,
+                offset: Offset(0, 2),
+                blurRadius: 6.0,
+              ),
+            ],
+          ),
+          child: CircleAvatar(
+            child: ClipOval(
+              child: Image(
+                height: 50.0,
+                width: 50.0,
+                image: AssetImage(comments[index].authorImageUrl),
+                fit: BoxFit.cover,
+              ),
+            ),
+          ),
+        ),
+        title: Text(
+          comments[index].authorName,
+          style: TextStyle(fontWeight: FontWeight.bold),
+        ),
+        subtitle: Text(comments[index].text),
+        trailing: IconButton(
+          onPressed: () => print('Like comment'),
+          icon: Icon(Icons.favorite_border),
+          color: Colors.grey,
+        ),
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Color(0xFFEDF0F6),
+      body: SingleChildScrollView(
+        physics: AlwaysScrollableScrollPhysics(),
+        child: Column(
+          children: [
+            Container(
+              padding: EdgeInsets.only(top: 40.0),
+              width: double.infinity,
+              height: 600.0,
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.circular(25.0),
+              ),
+              child: Column(
+                children: [
+                  Padding(
+                    padding: EdgeInsets.symmetric(vertical: 10.0),
+                    child: Column(
+                      children: [
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            IconButton(
+                              onPressed: () => Navigator.pop(context),
+                              icon: Icon(Icons.arrow_back),
+                              iconSize: 30.0,
+                              color: Colors.black,
+                            ),
+                            Container(
+                              width: MediaQuery.of(context).size.width * 0.8,
+                              child: ListTile(
+                                leading: Container(
+                                  width: 50.0,
+                                  height: 50.0,
+                                  decoration: BoxDecoration(
+                                    shape: BoxShape.circle,
+                                    boxShadow: [
+                                      BoxShadow(
+                                        color: Colors.black45,
+                                        offset: Offset(0, 2),
+                                        blurRadius: 6.0,
+                                      ),
+                                    ],
+                                  ),
+                                  child: CircleAvatar(
+                                    child: ClipOval(
+                                      child: Image(
+                                        height: 50.0,
+                                        width: 50.0,
+                                        image: AssetImage(
+                                            widget.post.authorImageUrl),
+                                        fit: BoxFit.cover,
+                                      ),
+                                    ),
+                                  ),
+                                ),
+                                title: Text(
+                                  widget.post.authorName,
+                                  style: TextStyle(fontWeight: FontWeight.bold),
+                                ),
+                                subtitle: Text(widget.post.timeAgo),
+                                trailing: IconButton(
+                                  onPressed: () => print('More'),
+                                  icon: Icon(Icons.more_horiz),
+                                  color: Colors.black,
+                                ),
+                              ),
+                            ),
+                          ],
+                        ),
+                        InkWell(
+                          onDoubleTap: () => print('Like post'),
+                          child: Container(
+                            margin: EdgeInsets.all(10.0),
+                            width: double.infinity,
+                            height: 400.0,
+                            decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(25.0),
+                              boxShadow: [
+                                BoxShadow(
+                                  color: Colors.black45,
+                                  offset: Offset(0, 5),
+                                  blurRadius: 8.0,
+                                ),
+                              ],
+                              image: DecorationImage(
+                                image: AssetImage(widget.post.imageUrl),
+                                fit: BoxFit.fitWidth,
+                              ),
+                            ),
+                          ),
+                        ),
+                        Padding(
+                          padding: EdgeInsets.symmetric(horizontal: 20.0),
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              Row(
+                                children: [
+                                  Row(
+                                    children: [
+                                      IconButton(
+                                        onPressed: () => print('Like post'),
+                                        icon: Icon(Icons.favorite_border),
+                                        iconSize: 30.0,
+                                      ),
+                                      Text(
+                                        '2,515',
+                                        style: TextStyle(
+                                          fontSize: 14.0,
+                                          fontWeight: FontWeight.w600,
+                                        ),
+                                      ),
+                                    ],
+                                  ),
+                                  SizedBox(height: 20.0),
+                                  Row(
+                                    children: [
+                                      IconButton(
+                                        onPressed: () {
+                                          print('Chat');
+                                        },
+                                        icon: Icon(Icons.chat),
+                                        iconSize: 30.0,
+                                      ),
+                                      Text(
+                                        '254',
+                                        style: TextStyle(
+                                          fontSize: 14.0,
+                                          fontWeight: FontWeight.w600,
+                                        ),
+                                      ),
+                                    ],
+                                  ),
+                                ],
+                              ),
+                              IconButton(
+                                onPressed: () => print('Save post'),
+                                icon: Icon(Icons.bookmark_border),
+                                iconSize: 30.0,
+                              ),
+                            ],
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            SizedBox(height: 10.0),
+            Container(
+              width: double.infinity,
+              height: 600.0,
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.only(
+                  topLeft: Radius.circular(30.0),
+                  topRight: Radius.circular(30.0),
+                ),
+              ),
+              child: Column(
+                children: [
+                  _buildComment(0),
+                  _buildComment(1),
+                  _buildComment(2),
+                  _buildComment(3),
+                  _buildComment(4),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+      bottomNavigationBar: Transform.translate(
+        offset: Offset(0.0, -1 * MediaQuery.of(context).viewInsets.bottom),
+        child: Container(
+          height: 100.0,
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.only(
+              topLeft: Radius.circular(30.0),
+              topRight: Radius.circular(30.0),
+            ),
+            boxShadow: [
+              BoxShadow(
+                color: Colors.black12,
+                offset: Offset(0, -2),
+                blurRadius: 2.0,
+              ),
+            ],
+            color: Colors.white,
+          ),
+          child: Padding(
+            padding: EdgeInsets.all(12.0),
+            child: TextField(
+              decoration: InputDecoration(
+                border: InputBorder.none,
+                hintText: 'Add a comment',
+                enabledBorder: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(30.0),
+                  borderSide: BorderSide(
+                    color: Colors.grey,
+                  ),
+                ),
+                focusedBorder: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(30.0),
+                  borderSide: BorderSide(
+                    color: Colors.grey,
+                  ),
+                ),
+                contentPadding: EdgeInsets.all(20.0),
+                prefixIcon: Container(
+                  margin: EdgeInsets.all(4.0),
+                  width: 48.0,
+                  height: 48.0,
+                  decoration: BoxDecoration(
+                    shape: BoxShape.circle,
+                    boxShadow: [
+                      BoxShadow(
+                        color: Colors.black45,
+                        offset: Offset(0, 2),
+                        blurRadius: 6.0,
+                      ),
+                    ],
+                  ),
+                  child: CircleAvatar(
+                    child: ClipOval(
+                      child: Image(
+                        height: 48.0,
+                        width: 48.0,
+                        image: AssetImage(widget.post.authorImageUrl),
+                        fit: BoxFit.cover,
+                      ),
+                    ),
+                  ),
+                ),
+                suffixIcon: Container(
+                  margin: EdgeInsets.only(right: 4.0),
+                  width: 70.0,
+                  child: ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(30.0),
+                      ),
+                      primary: Color(0xFF23B66F),
+                    ),
+                    onPressed: () => print('Post comment'),
+                    child: Icon(
+                      Icons.send,
+                      color: Colors.white,
+                      size: 25.0,
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
